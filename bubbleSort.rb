7d57d434387824e3f9966d6f4ae7835135284d64
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-09-25 13:47:08 +0200

    finished

---
+++ bubbleSort.rb
@@ -0,0 +1,21 @@
+def bubble_sort (array)
+    n  = array.length
+    until n <= 1 do
+        newn = 0
+        1.upto(n-1) do |i|
+            if array[i - 1] > array[i]
+                aux = array[i]
+                array[i] = array[i -1]
+                array[i-1] = aux
+                newn = i
+            end
+        end
+        n = newn
+    end
+    array
+end
+
+
+numbers = [4,3,78,2,0,2]
+puts "Numbers: #{numbers}"
+puts "Sorted numbers: #{bubble_sort(numbers)}"
